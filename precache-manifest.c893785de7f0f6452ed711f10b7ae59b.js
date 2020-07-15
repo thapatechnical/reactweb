3e3bd916f68669f743e54d4bd6410cb3117b4d60
thapatechnical <vbthapa55@gmail.com>
2020-07-15 12:03:16 +0530

    Updates

---
+++ precache-manifest.c893785de7f0f6452ed711f10b7ae59b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7b889c9f3686c9af2ed516e45f4f36",
+    "url": "/reactweb/index.html"
+  },
+  {
+    "revision": "cb7bd3f7bf14631d295a",
+    "url": "/reactweb/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "ec81a3c25d6ed62fcff0",
+    "url": "/reactweb/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "cb7bd3f7bf14631d295a",
+    "url": "/reactweb/static/js/2.717f1fdb.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactweb/static/js/2.717f1fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec81a3c25d6ed62fcff0",
+    "url": "/reactweb/static/js/main.97d30404.chunk.js"
+  },
+  {
+    "revision": "4fb589a01037be49ea6c",
+    "url": "/reactweb/static/js/runtime-main.10959dc0.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/reactweb/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/reactweb/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/reactweb/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/reactweb/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/reactweb/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/reactweb/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/reactweb/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/reactweb/static/media/software.43049cae.jpeg"
+  }
+]);
